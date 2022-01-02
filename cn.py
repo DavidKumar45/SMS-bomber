be2fb60ccd02fb4d97923f5579758aa312692b30
DavidKumar45 <90750659+DavidKumar45@users.noreply.github.com>
2022-01-02 09:57:19 +0100

    Add files via upload

---
+++ cn.py
@@ -0,0 +1,23 @@
+import requests
+
+from requests.structures import CaseInsensitiveDict
+
+import os
+
+import sys
+
+import time
+
+
+os.system("clear")
+
+red="\033[0;31m"          # Red
+
+yellow="\033[0;33m"       # Yellow
+
+
+
+#cn.py
+
+print(red+"##################\n =============================\n Creatot : NWOMBU DAVID CHINEDU    \n =============================\n Facebook : www.facebook.com/profile.php?id=100063716716333
+\n =============================\n Modifier : Nwombu DAVID\n =============================\n GITHUB : https://github.com/DavidKumar45\n =============================\n FB Page: www.facebook.com/Robert. Micheal\n==========================================================")
